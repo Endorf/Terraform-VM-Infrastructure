b4f745c6893693669dbac6f779548429ec6d40d1
Dmytro Kos <dmytro.kosolapenko@gmail.com>
2023-05-26 19:56:33 +0300

    test fmt command

--- main.tf
+++ main.tf
@@ -9,14 +9,14 @@ terraform {
 
   required_providers {
     aws = {
-      source       = "hashicorp/aws"
-      version      = "~> 4.67.0"
+      source  = "hashicorp/aws"
+      version = "~> 4.67.0"
     }
   }
 }
 
 provider "aws" {
-  region           = "us-east-1"
+  region = "us-east-1"
 }
 
 locals {
@@ -24,15 +24,15 @@ locals {
 }
 
 module "application_web" {
-  source           = "./modules/app"
+  source = "./modules/app"
 
   # Input Variables
-  bucket_prefix    = "web-app-${ local.environment_name }"
+  bucket_prefix    = "web-app-${local.environment_name}"
   app_name         = "web-app"
-  environment_name = "${ local.environment_name }"
+  environment_name = local.environment_name
   create_dns_zone  = terraform.workspace == "live" ? true : false
   instance_type    = "t3.micro"
-  db_name          = "web-app-${ local.environment_name }-db"
+  db_name          = "web-app-${local.environment_name}-db"
   db_user          = var.db_user
   db_pass          = var.db_pass
 }
