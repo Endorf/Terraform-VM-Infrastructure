d173704ba6727fe16f4980610d9f12d5f4cad479
Dmytro Kos <dmytro.kosolapenko@gmail.com>
2023-05-26 16:54:02 +0300

    add app module

---
+++ modules/app/variables.tf
@@ -0,0 +1,57 @@
+# Environment
+
+variable "environment_name" {
+  description = "Deployment environment (dev/stage/live)"
+  type        = string
+  default     = "dev"
+}
+
+variable "app_name" {
+  description = "Web application"
+  type        = string
+  default     = "web-app"
+}
+
+
+# EC2
+
+variable "ami" {
+  description = "amazon ec2 instance"
+  type        = string
+  default     = "ami-0715c1897453cabd1"
+}
+
+variable "instance_type" {
+  description = "ec2 instance type"
+  type        = string
+  default     = "t3.micro"
+}
+
+
+# S3
+
+variable "bucket_prefix" {
+  description = "prefix for s3 app bucket"
+  type        = string
+}
+
+
+# RDS
+
+variable "db_name" {
+  description = "database name"
+  type        = string
+  default     = "app_db"
+}
+
+variable "db_user" {
+  description = "database username"
+  type        = string
+  default     = "admin"
+}
+
+variable "db_pass" {
+  description = "database password"
+  type        = string
+  sensitive   = true
+}
