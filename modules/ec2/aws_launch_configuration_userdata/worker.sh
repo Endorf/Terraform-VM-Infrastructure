feef998e9c372b756a5a9451ca29078bbf1d4422
Dmytro Kos <dmytro.kosolapenko@gmail.com>
2023-06-12 13:32:07 +0300

    fix issue

--- modules/ec2/aws_launch_configuration_userdata/worker.sh
+++ modules/ec2/aws_launch_configuration_userdata/worker.sh
@@ -3,7 +3,7 @@ yum -y update
 yum -y install ruby
 yum -y install wget
 
-cd /home/ec2-user
+cd /home/ec2-user || exit
 wget https://aws-codedeploy-us-east-1.s3.us-east-1.amazonaws.com/latest/install
 chmod +x ./install
 ./install auto
